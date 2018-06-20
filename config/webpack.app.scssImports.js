90661902d3e3a9c635e07daa038d70cd7c5f4c99
Ge11ert <gellert94@mail.ru>
2018-06-20 23:31:32 +0300

    Add typographic styles

--- config/webpack.app.scssImports.js
+++ config/webpack.app.scssImports.js
@@ -1,6 +1,6 @@
 module.exports = [
   'app/common/utils/stylesheets/variables/colors',
+  'app/common/utils/stylesheets/variables/typography',
   // 'app/common/utils/stylesheets/variables/breakpoints',
-  // 'app/common/utils/stylesheets/variables/typography',
   // 'app/common/utils/stylesheets/mixins/text-cut',
 ];
