41e3a3b9bf495042a17155d0fb919b2043997f9a
Ge11ert <gellert94@mail.ru>
2018-06-21 00:54:32 +0300

    Add sandbox-table

---
+++ src/sandbox/components/sandbox-table/sandbox-table.jsx
@@ -0,0 +1,31 @@
+const SandboxTable = (props) => {
+
+  const buildTableData = () => {
+    return props.data.map((row, i) => {
+      return (
+        <tr key={`row-${i}`} className="sandbox-table__row">
+          {row.map((cell, j) => (
+            <td
+              key={`cell-${i}${j}`}
+              className="sandbox-table__cell"
+            >{cell}</td>
+          ))}
+        </tr>
+      )
+    });
+  };
+
+  return (
+    <table className={b('sandbox-table', props)}>
+      <tbody>
+      {buildTableData()}
+      </tbody>
+    </table>
+  );
+};
+
+SandboxTable.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.array),
+};
+
+export default SandboxTable;
