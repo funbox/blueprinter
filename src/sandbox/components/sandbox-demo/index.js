700daff9511c9d922c9dffc5a384dd949b8af095
Ge11ert <gellert94@mail.ru>
2018-06-22 11:49:51 +0300

    Add side menu to the sandbox

--- src/sandbox/components/sandbox-demo/index.js
+++ src/sandbox/components/sandbox-demo/index.js
@@ -4,3 +4,5 @@ require('./sandbox-demo.sass');
 
 require('./_theme/_standard/sandbox-demo_theme_standard.scss');
 require('./_theme/_air/sandbox-demo_theme_air.scss');
+
+require('./_for/_side-menu/sandbox-demo_for_side-menu.sass');
--- src/sandbox/views/sandbox-side-menu/sandbox-side-menu.jsx
+++ src/sandbox/views/sandbox-side-menu/sandbox-side-menu.jsx
@@ -5,10 +5,13 @@ import SandboxTable from 'sandbox/components/sandbox-table';
 import InlineCode from 'sandbox/components/inline-code';
 
 import MethodBadge from 'app/components/method-badge';
+import SideMenu from 'app/components/side-menu';
+import ResourceGroup from 'app/components/resource-group';
+
+import data from 'app/mocks/data';
 
 export default class SandboxSideMenu extends React.Component {
   render() {
-
     const httpMethods = ['delete', 'get', 'head', 'options', 'patch', 'post', 'put'];
     const badgeTableRows = [
       httpMethods,
@@ -28,6 +31,16 @@ export default class SandboxSideMenu extends React.Component {
               data={badgeTableRows}
               mods={{ for: 'method-badges' }}
             />
+          </SandboxDemo>
+        </SandboxSection>
+
+        <SandboxSection id="resource-group-block" title="Блок Resource group бокового меню">
+          <ResourceGroup group={data[0]}/>
+        </SandboxSection>
+
+        <SandboxSection id="side-menu" title="Боковое меню">
+          <SandboxDemo mods={{ for: 'side-menu' }}>
+            <SideMenu data={data}/>
           </SandboxDemo>
         </SandboxSection>
       </div>
