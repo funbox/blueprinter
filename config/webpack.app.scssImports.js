62c6a2ebd84919d811bbaf950365092edcf724e9
Ge11ert <gellert94@mail.ru>
2018-06-22 18:20:25 +0300

    Add variables with the base padding

--- config/webpack.app.scssImports.js
+++ config/webpack.app.scssImports.js
@@ -1,6 +1,6 @@
 module.exports = [
   'app/common/utils/stylesheets/variables/colors',
   'app/common/utils/stylesheets/variables/typography',
+  'app/common/utils/stylesheets/variables/box-model',
   'app/common/utils/stylesheets/mixins/visually-hidden',
-  // 'app/common/utils/stylesheets/variables/breakpoints',
 ];
