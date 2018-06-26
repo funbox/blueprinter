67cf230aefcac2a5673e6eeaee6b10679ad334d6
Ge11ert <gellert94@mail.ru>
2018-06-26 11:50:18 +0300

    Add examples of an attributes list to the sandbox

--- src/sandbox/views/sandbox-request-response-block/sandbox-request-response-block.jsx
+++ src/sandbox/views/sandbox-request-response-block/sandbox-request-response-block.jsx
@@ -4,7 +4,17 @@ import SandboxParagraph from 'sandbox/components/sandbox-paragraph';
 import InlineCode from 'sandbox/components/inline-code';
 
 import CodeSnippet from 'app/components/code-snippet';
+import AttributesList from 'app/components/attirbutes-list';
+
 import { body, schema, rawBody } from 'app/mocks/json';
+import {
+  arrayAttribute,
+  arrayOfObjects,
+  objectAttribute,
+  complexObject,
+  singleAttribute,
+  attributeBlock,
+} from 'app/mocks/attributes';
 
 export default class SandboxRequestResponseBlock extends React.Component {
   render() {
@@ -32,6 +42,42 @@ export default class SandboxRequestResponseBlock extends React.Component {
             <CodeSnippet mods={{ disabledSyntax: true }}>
               {rawBody}
             </CodeSnippet>
+          </SandboxDemo>
+        </SandboxSection>
+
+        <SandboxSection id="single-attribute" title="Одиночный атрибут">
+          <SandboxDemo mods={{ for: 'attributes-list' }}>
+            <AttributesList attributes={[singleAttribute]}/>
+          </SandboxDemo>
+        </SandboxSection>
+
+        <SandboxSection id="object-attribute" title="Атрибут-объект">
+          <SandboxDemo mods={{ for: 'attributes-list' }}>
+            <AttributesList attributes={[objectAttribute]}/>
+          </SandboxDemo>
+        </SandboxSection>
+
+        <SandboxSection id="array-attribute" title="Атрибут-массив">
+          <SandboxDemo mods={{ for: 'attributes-list' }}>
+            <AttributesList attributes={[arrayAttribute]}/>
+          </SandboxDemo>
+        </SandboxSection>
+
+        <SandboxSection id="complex-object" title="Вложенный объект">
+          <SandboxDemo mods={{ for: 'attributes-list' }}>
+            <AttributesList attributes={[complexObject]}/>
+          </SandboxDemo>
+        </SandboxSection>
+
+        <SandboxSection id="array-of-objects" title="Массив объектов">
+          <SandboxDemo mods={{ for: 'attributes-list' }}>
+            <AttributesList attributes={[arrayOfObjects]}/>
+          </SandboxDemo>
+        </SandboxSection>
+
+        <SandboxSection id="attributes-block" title="Блок атрибутов">
+          <SandboxDemo mods={{ for: 'attributes-list' }}>
+            <AttributesList attributes={attributeBlock}/>
           </SandboxDemo>
         </SandboxSection>
       </div>
