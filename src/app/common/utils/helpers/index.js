311b57639600bb7ef3a6cb6c497526e5ab367f8b
Ge11ert <gellert94@mail.ru>
2018-06-28 12:56:21 +0300

    Add helpers to process attributes

--- src/app/common/utils/helpers/index.js
+++ src/app/common/utils/helpers/index.js
@@ -2,6 +2,46 @@ const extractTransactionMethod = transition => (
   transition.content[0].content[0].attributes.method.content
 );
 
+const extractAttributeData = (attribute, disabledExample = false) => {
+  const { key, value } = attribute.content;
+  let attributeKey;
+  let attributeType;
+  let attributeExample;
+
+  if (key && value) {
+    attributeKey = key.content || null;
+    attributeType = value.element || null;
+    attributeExample = (!disabledExample && value.content) || null;
+  } else {
+    attributeType = attribute.element || null;
+    attributeExample = (!disabledExample && attribute.content) || null;
+  }
+
+  const attributeDescription = (!!attribute.meta && attribute.meta.description.content) || null;
+  const attributeProps = (!!attribute.attributes && attribute.attributes.typeAttributes[0].content) || null;
+
+  return { attributeKey, attributeType, attributeExample, attributeDescription, attributeProps };
+};
+
+const getAttributeChildren = attribute => {
+  const complexTypes = ['array', 'enum', 'object'];
+  const childrenByType = {
+    array: (attr) => (Array.isArray(attr.content) ? attr.content : attr.content.value.content),
+    enum: (attr) => attr.content.value.attributes.enumerations.content,
+    object: (attr) => (Array.isArray(attr.content) ? attr.content : attr.content.value.content),
+  };
+
+  const { value } = attribute.content;
+  const attributeType = value ? value.element : attribute.element;
+
+  if (!complexTypes.includes(attributeType)) {
+    return [];
+  }
+  return childrenByType[attributeType](attribute);
+};
+
 export {
   extractTransactionMethod,
+  extractAttributeData,
+  getAttributeChildren,
 };
