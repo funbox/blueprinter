f8698d4e0ae7f100bdf4832cc87311efc768435d
Ge11ert <gellert94@mail.ru>
2018-07-25 12:22:14 +0300

    Update the mock with transition data

--- src/app/mocks/request.js
+++ src/app/mocks/request.js
@@ -4,25 +4,45 @@ import singleAttribute from './attributes/single-attribute';
 const body = require('./json/body.json');
 const schema = require('./json/schema.json');
 
-export default [
-  {
-    request: {
-      headers: [{ key: 'X-NOKIA-MSISDN', value: '79173332211' }],
-      attributes: [singleAttribute],
-    },
-    response: {
-      statusCode: 200,
-      headers: [
-        { key: 'Content-Type', value: 'application/json' },
-        { key: 'X-Auth-Token', value: 'Session token' }],
-      attributes: attributesBlock,
-      description: 'Custom transaction description',
-      body,
-      schema,
-    },
-    attributes: {
-      method: 'POST',
-      uri: '/users/{id}?country=ru&active=true&votes=0',
-    },
+const transaction1 = {
+  request: {
+    title: 'With headers',
+    headers: [{ key: 'X-NOKIA-MSISDN', value: '79173332211' }],
+    attributes: [singleAttribute],
   },
+  response: {
+    statusCode: 200,
+    headers: [
+      { key: 'Content-Type', value: 'application/json' },
+      { key: 'X-Auth-Token', value: 'Session token' }],
+    attributes: attributesBlock,
+    description: 'Custom transaction description',
+    body,
+    schema,
+  },
+  attributes: {
+    method: 'POST',
+    uri: '/users/{id}?country=ru&active=true&votes=0',
+  },
+};
+
+const transaction2 = {
+  request: {
+    title: 'With body',
+    body: 'Hello world',
+  },
+  response: {
+    statusCode: 401,
+    headers: [{ key: 'Content-Type', value: 'application/json' }],
+    attributes: attributesBlock,
+  },
+  attributes: {
+    method: 'POST',
+    uri: '/users/{id}?country=ru&active=true&votes=0',
+  },
+};
+
+export default [
+  transaction1,
+  transaction2,
 ];
