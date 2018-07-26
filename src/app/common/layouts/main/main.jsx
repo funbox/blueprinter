68fed40faa3ab206de835ff633e41a15679db1c3
Ge11ert <gellert94@mail.ru>
2018-07-26 17:07:06 +0300

    Add page layout elements to the main component

--- src/app/common/layouts/main/main.jsx
+++ src/app/common/layouts/main/main.jsx
@@ -1,13 +1,38 @@
+import Page, { Page__Body, Page__Aside, Page__Navigation, Page__Layout } from 'app/components/page';
+import SideMenu from 'app/components/side-menu';
+import MainContent from 'app/components/main-content';
+import Transition from 'app/components/transition';
+import ResourceGroupSection from 'app/components/resource-group-section';
+
+import groups from 'app/mocks/resource-group';
+import request from 'app/mocks/request';
+
 export default class Main extends React.Component {
   render() {
-    const {
-      children,
-    } = this.props;
-
     return (
-      <div>
-        {children}
-      </div>
+      <Page>
+        <Page__Layout>
+          <Page__Navigation>
+            <SideMenu data={groups}/>
+          </Page__Navigation>
+
+          <Page__Body>
+            <MainContent
+              title="API Blueprint"
+              description={<p>Документация проекта "Новый проект"</p>}
+            >
+              <ResourceGroupSection group={groups[0]}/>
+            </MainContent>
+          </Page__Body>
+
+          <Page__Aside>
+            <Transition
+              transactions={request.content}
+              attributes={request.attributes}
+            />
+          </Page__Aside>
+        </Page__Layout>
+      </Page>
     );
   }
 }
