9067b8dbd222647d71b84152c23b2fff8524c88c
Ge11ert <gellert94@mail.ru>
2018-07-26 12:17:28 +0300

    Add modifiers for the page title

--- src/app/components/page-title/index.js
+++ src/app/components/page-title/index.js
@@ -1,1 +1,3 @@
 export { default } from 'fb-base-blocks/page-title';
+
+require('./_for/_main-content/page-title_for_main-content.scss');
--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -1,2 +1,5 @@
 export * from 'fb-base-blocks/page';
 export { default } from 'fb-base-blocks/page';
+
+require('./__title/page__title.scss');
+require('./__description/page__description.scss');
