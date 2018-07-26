26425e30ba9abc5814419a00cb4825f2eca54a9f
Ge11ert <gellert94@mail.ru>
2018-07-26 19:41:28 +0300

    Add a wrapper component for resizable elements

---
+++ src/app/components/resizable/resizable.jsx
@@ -0,0 +1,58 @@
+import _Resizable from 're-resizable';
+
+const Resizable = (props) => {
+  const {
+    direction,
+    children,
+    minWidth,
+    maxWidth,
+  } = props;
+
+  const permittedDirections = {
+    top: false,
+    right: false,
+    bottom: false,
+    left: false,
+    topRight: false,
+    bottomRight: false,
+    bottomLeft: false,
+    topLeft: false,
+  };
+
+  if (typeof direction === 'string') {
+    permittedDirections[direction] = true;
+  }
+
+  if (Array.isArray(direction)) {
+    direction.forEach(dir => {
+      permittedDirections[dir] = true;
+    });
+  }
+
+  return !!children && (
+    <_Resizable
+      enable={permittedDirections}
+      className={b('resizable', props)}
+      minWidth={minWidth}
+      maxWidth={maxWidth}
+    >
+      {props.children}
+    </_Resizable>
+  );
+};
+
+Resizable.defaultProps = {
+  directions: ['left', 'right'],
+};
+
+Resizable.propTypes = {
+  children: PropTypes.node,
+  direction: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.string,
+  ]),
+  minWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  maxWidth: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+};
+
+export default Resizable;
