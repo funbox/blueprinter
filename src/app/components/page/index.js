229064c743027f58cc01477ad35c08764f09a90f
Ge11ert <gellert94@mail.ru>
2018-07-26 17:03:40 +0300

    Add styling of elements of the page block

--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -1,5 +1,12 @@
 export * from 'fb-base-blocks/page';
 export { default } from 'fb-base-blocks/page';
 
-require('./__title/page__title.scss');
+export { default as Page__Navigation } from './__navigation/page__navigation';
+export { default as Page__Layout } from './__layout/page__layout';
+
+require('./__aside/page__aside.scss');
+require('./__body/page__body.scss');
 require('./__description/page__description.scss');
+require('./__layout/page__layout.scss');
+require('./__navigation/page__navigation.scss');
+require('./__title/page__title.scss');
