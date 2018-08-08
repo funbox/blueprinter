e38ab2e752acdd492c978a2c349882adbd455f86
Ge11ert <gellert94@mail.ru>
2018-08-08 16:55:24 +0300

    Refactor color variables usage

--- config/webpack.app.scssImports.js
+++ config/webpack.app.scssImports.js
@@ -1,5 +1,5 @@
 module.exports = [
-  'app/common/utils/stylesheets/variables/colors',
+  'app/common/utils/stylesheets/variables/colors/colors',
   'app/common/utils/stylesheets/variables/typography',
   'app/common/utils/stylesheets/variables/box-model',
   'app/common/utils/stylesheets/mixins/visually-hidden',
