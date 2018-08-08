14965153a9427adf30fd64c5e9efa28d47cc241b
Ge11ert <gellert94@mail.ru>
2018-08-08 14:06:39 +0300

    Delete main layout component

--- src/app/components/app/app.jsx
+++ src/app/components/app/app.jsx
@@ -1,14 +1,7 @@
-import { Route } from 'react-router-dom';
-
-import MainLayout from 'app/common/layouts/main';
 import Home from 'app/views/home';
 
 export default class App extends React.Component {
   render() {
-    return (
-      <MainLayout>
-        <Route exact path="/" component={Home}/>
-      </MainLayout>
-    );
+    return <Home/>;
   }
 }
