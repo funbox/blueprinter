fd00a155f0e723df9c3adfc44e99465ab911aad8
Ge11ert <gellert94@mail.ru>
2018-08-10 12:55:57 +0300

    Set reference to the element with the main content

--- src/app/components/main-content/main-content.jsx
+++ src/app/components/main-content/main-content.jsx
@@ -2,32 +2,41 @@ import RawContent from 'app/components/raw-content';
 import { Page__Main, Page__Title, Page__Content } from 'app/components/page';
 import PageTitle from 'app/components/page-title';
 
-const MainContent = (props) => {
-  const {
-    title,
-    description,
-    children,
-  } = props;
-  return (
-    <Page__Main>
-      <Page__Title>
-        <PageTitle
-          mods={{ for: 'main-content' }}
-        >{title}</PageTitle>
-      </Page__Title>
+class MainContent extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <Page__Content>
-        <RawContent mix="page__description">
-          {description}
-        </RawContent>
-      </Page__Content>
+    this.mainContent = React.createRef();
+  }
 
-      <Page__Content>
-        {children}
-      </Page__Content>
-    </Page__Main>
-  );
-};
+  render() {
+    const {
+      title,
+      description,
+      children,
+    } = this.props;
+
+    return (
+      <Page__Main>
+        <Page__Title>
+          <PageTitle
+            mods={{ for: 'main-content' }}
+          >{title}</PageTitle>
+        </Page__Title>
+
+        <Page__Content>
+          <RawContent mix="page__description">
+            {description}
+          </RawContent>
+        </Page__Content>
+
+        <Page__Content myRef={this.mainContent}>
+          {children}
+        </Page__Content>
+      </Page__Main>
+    );
+  }
+}
 
 MainContent.defaultProps = {
   title: 'API',
--- src/fb-base-blocks/page/__content/page__content.jsx
+++ src/fb-base-blocks/page/__content/page__content.jsx
@@ -1,11 +1,15 @@
 const Page__Content = (props) => (
-  <div className={b('page__content', props)}>
+  <div
+    className={b('page__content', props)}
+    ref={props.myRef}
+  >
     {props.children}
   </div>
 );
 
 Page__Content.propTypes = {
   children: PropTypes.node,
+  myRef: PropTypes.object,
 };
 
 export default Page__Content;
