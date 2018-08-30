9da3a5121dc1370101278861d17a13437766dffd
Ge11ert <gellert94@mail.ru>
2018-08-30 13:58:41 +0300

    Add dev-server config

---
+++ config/webpack.app.serve.js
@@ -0,0 +1,11 @@
+/**
+ * Конфигурация Webpack для сборки окружения разработки приложения.
+ */
+module.exports = require('./webpack.app.config')({
+  build: false,
+  test: false,
+  skipUglify: true,
+  disableHmre: true,
+  disableCssExtracting: true,
+  createRebuildInProgressFile: true,
+});
