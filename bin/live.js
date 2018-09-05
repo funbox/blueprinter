17e682edca9d31c3958375f76da1ddd022608f06
Ge11ert <gellert94@mail.ru>
2018-09-05 14:24:02 +0300

    Use browser-sync in dev-mode

--- bin/live.js
+++ bin/live.js
@@ -1,30 +1,21 @@
-const chokidar = require('chokidar');
-const fs = require('fs');
-const path = require('path');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
 const { isPortFree } = require('funbox-free-port-finder');
+
+const browserSync = require('browser-sync').create();
 
 const { BASE_PATH, renderRefract } = require('./main');
 
-const buildConfig = require(path.resolve(BASE_PATH, 'config/webpack.app.serve.js'));
-const serverOptions = buildConfig.devServer;
-
-let resolveInitialBuild;
-
-serverOptions.hot = true;
-serverOptions.inline = true;
-serverOptions.stats = 'errors-only';
-
-delete buildConfig.devServer;
+const serverParams = {
+  server: `${BASE_PATH}/static`,
+  injectChanges: false,
+};
 
 const renderAndServe = async (inputFileName, port, host, options, callback) => {
   const watchSource = (fileName) => {
-    const watcher = chokidar.watch(fileName);
+    const watcher = browserSync.watch(fileName);
 
     watcher.on('change', (path) => {
       console.log(`Updated ${path}`);
-      renderRefract(inputFileName, options, callback);
+      renderRefract(inputFileName, options, callback).then(() => browserSync.reload());
     })
   };
 
@@ -42,32 +33,15 @@ const renderAndServe = async (inputFileName, port, host, options, callback) => {
   }
 };
 
-// В данном примере для реализации работы live-режима использует плагин funbox-rebuild-in-progress-webpack-plugin
-const rebuildInProgressFile = path.resolve(__dirname, '../node_modules/.rebuildInProgress');
-
-fs.watch(path.dirname(rebuildInProgressFile), (eventType, filename) => {
-  if (eventType === 'rename' && filename === path.basename(rebuildInProgressFile)) {
-    if (fs.existsSync(rebuildInProgressFile)) {
-      // Сборка началась
-    } else {
-      if (resolveInitialBuild) {
-        resolveInitialBuild();
-        resolveInitialBuild = null;
-      }
-    }
-  }
-});
-
 function startServer(port, host) {
   console.log(`Starting server at ${host}:${port}`);
-  return new Promise(resolve => {
-    new WebpackDevServer(webpack(buildConfig), serverOptions).listen(port, host, (err) => {
-      if (err) {
-        console.log(`Ошибка сборки: ${err}`);
-        process.exit(1);
-      }
+  serverParams.host = host;
+  serverParams.port = port;
 
-      resolveInitialBuild = resolve;
+  return new Promise(resolve => {
+    browserSync.init(serverParams, () => {
+      console.log('Server started');
+      resolve();
     });
   });
 }
