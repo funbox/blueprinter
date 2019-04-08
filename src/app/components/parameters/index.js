7a932bdd14b5f7531c93b42e04e202c9b08c1820
Irina Romanova <irina.tardiva@gmail.com>
2019-04-08 16:30:15 +0300

    Improve parameter name display

--- src/app/components/parameters/index.js
+++ src/app/components/parameters/index.js
@@ -5,6 +5,5 @@ require('./__content/parameters__content.scss');
 require('./_example/parameters__example.scss');
 require('./__item/parameters__item.scss');
 require('./__item-title/parameters__item-title.scss');
-require('./__item-content/parameters__item-content.scss');
 require('./__title/parameters__title.scss');
 require('./__collapse-button/parameters__collapse-button.scss');
