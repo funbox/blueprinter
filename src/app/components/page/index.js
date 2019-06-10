49e0cd448e24248e83c62acfd6653bb06970dfb2
Ge11ert <gellert94@mail.ru>
2019-06-10 18:27:25 +0300

    Add styling of the error page

--- src/app/components/page/index.js
+++ src/app/components/page/index.js
@@ -13,3 +13,5 @@ require('./__stripe/_for/_transition/page__stripe_for_transition.scss');
 require('./__stripe/_for/_aside-placeholder/page__stripe_for_aside-placeholder.scss');
 require('./__stripe/_for/_api-host/page__stripe_for_api-host.scss');
 require('./__title/page__title.scss');
+
+require('./_type/_error/page_type_error.scss');
