e519f66cd3c3d33c4935b4b11f4dd213fb268a27
Ge11ert <gellert94@mail.ru>
2019-08-07 17:22:47 +0300

    Use functional component for API Host

--- src/app/components/api-host/api-host.jsx
+++ src/app/components/api-host/api-host.jsx
@@ -1,31 +1,18 @@
-class ApiHost extends React.Component {
-  constructor(props) {
-    super(props);
+const ApiHost = (props) => {
+  const host = props.host.trim();
+  const href = /^(http|https):\/\/.+$/.exec(host) ? host : `http://${host}`;
 
-    this.apiHostRef = React.createRef();
-  }
-
-  componentDidMount() {
-    const blockHeight = this.apiHostRef.current.offsetHeight;
-    document.documentElement.style.setProperty('--page-description-height', `${blockHeight}px`);
-  }
-
-  render() {
-    const { host } = this.props;
-    const href = /^(http|https):\/\/.+$/.exec(host) ? host : `http://${host}`;
-
-    return (
-      <div className={b('api-host', this.props)} ref={this.apiHostRef}>
-        <h5 className="api-host__title">
-          API Endpoint
-        </h5>
-        <a className="api-host__link" href={href}>
-          {host}
-        </a>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={b('api-host', props)}>
+      <h5 className="api-host__title">
+        API Host
+      </h5>
+      <a className="api-host__link" href={href}>
+        {host}
+      </a>
+    </div>
+  );
+};
 
 ApiHost.propTypes = {
   host: PropTypes.string,
