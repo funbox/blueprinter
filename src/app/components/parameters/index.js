f2ba6ecbd57bd31c4b64afcd84c1cead8bee7ce2
Ge11ert <gellert94@mail.ru>
2019-08-09 16:53:42 +0300

    Use parameters table in the URI Parameters section

--- src/app/components/parameters/index.js
+++ src/app/components/parameters/index.js
@@ -1,9 +1,4 @@
 export { default } from './parameters';
 
-require('./__heading/parameters__heading.scss');
 require('./__content/parameters__content.scss');
-require('./_example/parameters__example.scss');
-require('./__item/parameters__item.scss');
-require('./__item-title/parameters__item-title.scss');
 require('./__title/parameters__title.scss');
-require('./__collapse-button/parameters__collapse-button.scss');
--- src/app/components/parameters/parameters.jsx
+++ src/app/components/parameters/parameters.jsx
@@ -1,7 +1,5 @@
-import RawContent from 'app/components/raw-content';
 import PropTypes from 'prop-types';
-
-import { get, htmlFromText } from 'app/common/utils/helpers';
+import ParametersTable, { parameterProps } from 'app/components/parameters-table';
 
 class Parameters extends React.Component {
   render() {
@@ -17,88 +15,7 @@ class Parameters extends React.Component {
           </h5>
         </div>
         <div className="parameters__content">
-          {params.map((param, index) => {
-            const title = get('meta', 'title', 'content').from(param);
-            const description = get('meta', 'description', 'content').from(param);
-            const defaultValue = get('content', 'value', 'attributes', 'default', 'content').from(param);
-            const valueType = get('content', 'value', 'element').from(param);
-            let example = get('content', 'value', 'content').from(param);
-            const choices = get('content', 'value', 'attributes', 'enumerations', 'content').from(param);
-
-            if (valueType === 'enum') {
-              example = get('attributes', 'samples').from(param);
-            }
-
-            const exampleValue = Array.isArray(example) ? example.map(exItem => exItem.content).join(', ') : example;
-
-            return (
-              <div className="parameters__item" key={index * 2}>
-                <div className="parameters__item-title" key={index * 2}>
-                  {param.content.key.content}
-                </div>
-                <div className="parameters__item-content" key={index * 2 + 1}>
-                  <RawContent>
-                    <code>
-                      {title || 'string'}
-                    </code>
-                    &nbsp;
-                    {param.attributes.typeAttributes.content.map((attr, attrIndex) => (
-                      <span key={attrIndex}>
-                        ({attr.content})
-                      </span>
-                    ))}
-                    &nbsp;
-                    {defaultValue && (
-                      <span className="parameters__default">
-                        <strong>
-                          Default:
-                        </strong>
-                        {' '}
-                        <span>
-                          {defaultValue}
-                        </span>
-                      </span>
-                    )}
-                    &nbsp;
-                    {example && (
-                      <span className="parameters__example">
-                        <strong>
-                          Example:
-                        </strong>
-                        {' '}
-                        <span>
-                          {exampleValue}
-                        </span>
-                      </span>
-                    )}
-                    &nbsp;
-                    {description && htmlFromText(description)}
-                    {choices && choices.length > 0 && (
-                      <div>
-                        <strong>
-                          Choices:
-                        </strong>
-                        {' '}
-                        <ul>
-                          {choices.map(choice => {
-                            const choiceDescription = get('meta', 'description', 'content').from(choice);
-                            return (
-                              <li key={`enum-member-${choice.content}`}>
-                                <code>
-                                  {choice.content}
-                                </code>
-                                {choiceDescription && ` \u2014 ${choiceDescription}`}
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      </div>
-                    )}
-                  </RawContent>
-                </div>
-              </div>
-            );
-          })}
+          <ParametersTable params={params}/>
         </div>
       </section>
     );
@@ -106,45 +23,7 @@ class Parameters extends React.Component {
 }
 
 Parameters.propTypes = {
-  params: PropTypes.arrayOf(PropTypes.shape({
-    meta: PropTypes.shape({
-      description: PropTypes.shape({
-        element: PropTypes.string,
-        content: PropTypes.string,
-      }),
-      title: PropTypes.shape({
-        element: PropTypes.string,
-        content: PropTypes.string,
-      }),
-    }),
-    attributes: PropTypes.shape({
-      typeAttributes: PropTypes.shape({
-        element: PropTypes.string,
-        content: PropTypes.array,
-      }),
-    }),
-    content: PropTypes.shape({
-      key: PropTypes.shape({
-        element: PropTypes.string,
-        content: PropTypes.string,
-      }),
-      value: PropTypes.shape({
-        element: PropTypes.string,
-        content: PropTypes.oneOfType([
-          PropTypes.string,
-          PropTypes.array,
-          PropTypes.shape({
-            attributes: PropTypes.shape({
-              enumerations: PropTypes.shape({
-                element: PropTypes.string,
-                content: PropTypes.array,
-              }),
-            }),
-          }),
-        ]),
-      }),
-    }),
-  })),
+  params: PropTypes.arrayOf(parameterProps),
 };
 
 export default Parameters;
