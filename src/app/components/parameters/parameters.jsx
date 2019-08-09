527d5617b8bb1da2982d70caefb0cdd5cc59c194
Ge11ert <gellert94@mail.ru>
2019-08-09 12:29:39 +0300

    Delete SlideToggle usage from URI Parameters section

--- src/app/components/parameters/parameters.jsx
+++ src/app/components/parameters/parameters.jsx
@@ -1,114 +1,87 @@
-import { SlideToggle } from 'react-slide-toggle';
 import RawContent from 'app/components/raw-content';
 import PropTypes from 'prop-types';
-import Button from 'fb-base-blocks/button';
 
 import { get, htmlFromText } from 'app/common/utils/helpers';
 
 class Parameters extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { collapsed: false };
-    this.toggleClass = this.toggleClass.bind(this);
-  }
-
-  toggleClass() {
-    this.setState(prevState => ({ ...prevState, collapsed: !prevState.collapsed }));
-  }
-
   render() {
     const { params } = this.props;
-    const { collapsed } = this.state;
+
+    if (params.length === 0) return null;
 
     return (
-      params.length > 0
-        ? (
-          <SlideToggle
-            bestPerformance
-            onCollapsed={this.toggleClass}
-            onExpanding={this.toggleClass}
-          >
-            {({ onToggle, setCollapsibleElement }) => (
-              <section className={b('parameters', { mods: { collapsed } })}>
-                <div className="parameters__heading">
-                  <h5 className="parameters__title">URI Parameters</h5>
-                  <Button mix={['parameters__collapse-button']} onClick={onToggle}>
-                    {this.state.collapsed ? 'Show' : 'Hide'}
-                  </Button>
-                </div>
-                <div className="parameters__content" ref={setCollapsibleElement}>
-                  {params.map((param, index) => {
-                    const title = get('meta', 'title', 'content').from(param);
-                    const description = get('meta', 'description', 'content').from(param);
-                    const defaultValue = get('content', 'value', 'attributes', 'default', 'content').from(param);
-                    const valueType = get('content', 'value', 'element').from(param);
-                    let example = get('content', 'value', 'content').from(param);
-                    const choices = get('content', 'value', 'attributes', 'enumerations', 'content').from(param);
+      <section className={b('parameters', this.props)}>
+        <div className="parameters__heading">
+          <h5 className="parameters__title">URI Parameters</h5>
+        </div>
+        <div className="parameters__content">
+          {params.map((param, index) => {
+            const title = get('meta', 'title', 'content').from(param);
+            const description = get('meta', 'description', 'content').from(param);
+            const defaultValue = get('content', 'value', 'attributes', 'default', 'content').from(param);
+            const valueType = get('content', 'value', 'element').from(param);
+            let example = get('content', 'value', 'content').from(param);
+            const choices = get('content', 'value', 'attributes', 'enumerations', 'content').from(param);
 
-                    if (valueType === 'enum') {
-                      example = get('attributes', 'samples').from(param);
+            if (valueType === 'enum') {
+              example = get('attributes', 'samples').from(param);
+            }
+
+            const exampleValue = Array.isArray(example) ? example.map(exItem => exItem.content).join(', ') : example;
+
+            return (
+              <div className="parameters__item" key={index * 2}>
+                <div className="parameters__item-title" key={index * 2}>{param.content.key.content}</div>
+                <div className="parameters__item-content" key={index * 2 + 1}>
+                  <RawContent>
+                    <code>{title || 'string'}</code>
+                    &nbsp;
+                    {param.attributes.typeAttributes.content.map((attr, attrIndex) => (
+                      <span key={attrIndex}>({attr.content})</span>
+                    ))}
+                    &nbsp;
+                    {defaultValue
+                    && (
+                      <span className="parameters__default">
+                        <strong>Default: </strong>
+                        <span>{defaultValue}</span>
+                      </span>
+                    )
                     }
-
-                    const exampleValue = Array.isArray(example) ? example.map(exItem => exItem.content).join(', ') : example;
-
-                    return (
-                      <div className="parameters__item" key={index * 2}>
-                        <div className="parameters__item-title" key={index * 2}>{param.content.key.content}</div>
-                        <div className="parameters__item-content" key={index * 2 + 1}>
-                          <RawContent>
-                            <code>{title || 'string'}</code>
-                            &nbsp;
-                            {param.attributes.typeAttributes.content.map((attr, attrIndex) => (
-                              <span key={attrIndex}>({attr.content})</span>
-                            ))}
-                            &nbsp;
-                            {defaultValue
-                              && (
-                                <span className="parameters__default">
-                                  <strong>Default: </strong>
-                                  <span>{defaultValue}</span>
-                                </span>
-                              )
-                            }
-                            &nbsp;
-                            {example
-                              && (
-                                <span className="parameters__example">
-                                  <strong>Example: </strong>
-                                  <span>{exampleValue}</span>
-                                </span>
-                              )
-                            }
-                            &nbsp;
-                            {description && htmlFromText(description)}
-                            {choices && choices.length > 0 && (
-                              <div>
-                                <strong>Choices: </strong>
-                                <ul>
-                                  {choices.map(choice => {
-                                    const choiceDescription = get('meta', 'description', 'content').from(choice);
-                                    return (
-                                      <li key={`enum-member-${choice.content}`}>
-                                        <code>{choice.content}</code>
-                                        {choiceDescription && ` \u2014 ${choiceDescription}`}
-                                      </li>
-                                    );
-                                  })}
-                                </ul>
-                              </div>
-                            )}
-                          </RawContent>
-                        </div>
+                    &nbsp;
+                    {example
+                    && (
+                      <span className="parameters__example">
+                        <strong>Example: </strong>
+                        <span>{exampleValue}</span>
+                      </span>
+                    )
+                    }
+                    &nbsp;
+                    {description && htmlFromText(description)}
+                    {choices && choices.length > 0 && (
+                      <div>
+                        <strong>Choices: </strong>
+                        <ul>
+                          {choices.map(choice => {
+                            const choiceDescription = get('meta', 'description', 'content').from(choice);
+                            return (
+                              <li key={`enum-member-${choice.content}`}>
+                                <code>{choice.content}</code>
+                                {choiceDescription && ` \u2014 ${choiceDescription}`}
+                              </li>
+                            );
+                          })}
+                        </ul>
                       </div>
-                    );
-                  })}
+                    )}
+                  </RawContent>
                 </div>
-              </section>
-            )}
-          </SlideToggle>
-        )
-        : null
+              </div>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 }
