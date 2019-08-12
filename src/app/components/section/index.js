b12fa29311c9c23acdea9bfe9e294041187fb1fc
Ge11ert <gellert94@mail.ru>
2019-08-12 15:55:02 +0300

    Update modifiers of the Section block

--- src/app/components/section/index.js
+++ src/app/components/section/index.js
@@ -2,4 +2,5 @@ export { default } from 'fb-base-blocks/section';
 
 require('./_hidden-title/section_hidden-title.scss');
 require('./_for/_transition/section_for_transition.scss');
+require('./_for/_transition-content/section_for_transition-content.scss');
 require('./_for/_uri-parameters/section_for_uri-parameters.scss');
