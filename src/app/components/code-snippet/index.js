1417f104fed489a2a16d8536b76de81c13684dee
Ge11ert <gellert94@mail.ru>
2019-08-12 15:56:51 +0300

    Update styling of the code-snippet block

--- src/app/components/code-snippet/index.js
+++ src/app/components/code-snippet/index.js
@@ -3,3 +3,6 @@ export { default } from './code-snippet';
 require('./__raw/code-snippet__raw.scss');
 require('./code-snippet.scss');
 require('./highlightjs.theme.scss');
+
+require('./_for/_asset/code-snippet_for_asset.scss');
+require('./_for/_headers/code-snippet_for_headers.scss');
