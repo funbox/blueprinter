50dec448ecd19b046e0ab1ddc344fde5c94b4097
Ge11ert <gellert94@mail.ru>
2019-08-12 17:37:02 +0300

    Use MessageContent in the MessageCard component

--- src/app/components/message-card/message-card.jsx
+++ src/app/components/message-card/message-card.jsx
@@ -1,5 +1,6 @@
 import RawContent from 'app/components/raw-content';
 import Link from 'app/components/link';
+import MessageContent from 'app/components/message-content';
 import { withHeaderAnchors } from 'app/common/utils/helpers';
 import { hashFromComment, createHash, combineHashes } from 'app/common/utils/helpers/hash';
 
@@ -41,23 +42,24 @@ const MessageCard = (props) => {
         )}
       </div>
 
-      {description && (
-        <div className="message-card__body">
+      <div className="message-card__body">
+        {description && (
           <RawContent mix="message-card__description">
             {withHeaderAnchors(description)}
           </RawContent>
-        </div>
-      )}
+        )}
+
+        <MessageContent message={message}/>
+      </div>
     </div>
   );
 };
 
 MessageCard.propTypes = {
   message: PropTypes.shape({
-    element: PropTypes.string,
-    meta: PropTypes.object,
-    content: PropTypes.array,
-    attributes: PropTypes.object,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    attributes: PropTypes.arrayOf(PropTypes.object),
   }),
   index: PropTypes.number.isRequired,
   location: PropTypes.object,
