86c2a4d824d8bc7b29f4b9daa17302dcafc1124e
Ge11ert <gellert94@mail.ru>
2019-08-14 16:22:15 +0300

    Declare default titles as constants

---
+++ src/app/constants/defaults.js
@@ -0,0 +1,4 @@
+export const API_DEFAULT_TITLE = 'API';
+export const GROUP_DEFAULT_TITLE = 'Resource Group';
+export const RESOURCE_DEFAULT_TITLE = 'Resource';
+export const MESSAGE_DEFAULT_TITLE = 'Message';
