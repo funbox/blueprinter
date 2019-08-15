d56b4418b5294d0f45dd73f084caef5f4b352c5d
Ge11ert <gellert94@mail.ru>
2019-08-15 12:29:53 +0300

    Add MainLayout and routes with content in the app.jsx

--- src/app/components/app/app.jsx
+++ src/app/components/app/app.jsx
@@ -1,27 +1,61 @@
+import { Switch } from 'react-router-dom';
 import parseSourceFile from 'app/common/utils/helpers/parseSourceFile';
 import sourceMock from 'app/source';
 
-import Home from 'app/views/home';
+import MainLayout from 'app/components/main-layout';
+import GroupRoutes from 'app/views/groups';
+import ResourceRoutes from 'app/views/resources';
+import ActionRoutes from 'app/views/actions';
 import Error from 'app/views/error';
 
 const source = window.refract || sourceMock;
 const parsedSource = parseSourceFile(source);
 
+const {
+  topLevelMeta,
+  groups,
+  resources,
+  actions,
+} = parsedSource;
+
 export default class App extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.location.pathname !== this.props.location.pathname) {
+      window.scrollTo(0, 0);
+    }
+  }
+
   render() {
-    return parsedSource.topLevelMeta.error ? (
-      <Error
-        error={parsedSource.topLevelMeta.error}
-        warnings={parsedSource.topLevelMeta.warnings}
-      />
-    ) : (
-      <Home parsedSource={parsedSource} location={this.props.location}/>
+    if (topLevelMeta.error) {
+      return (
+        <Error
+          error={topLevelMeta.error}
+          warnings={topLevelMeta.warnings}
+        />
+      );
+    }
+
+    const groupRoutes = GroupRoutes(groups);
+    const resourceRoutes = ResourceRoutes(resources);
+    const actionRoutes = ActionRoutes(actions);
+
+    const routes = groupRoutes.concat(resourceRoutes, actionRoutes);
+
+    return (
+      <MainLayout
+        topLevelMeta={topLevelMeta}
+        groups={groups}
+      >
+        <Switch>
+          {routes}
+        </Switch>
+      </MainLayout>
     );
   }
 }
 
 App.propTypes = {
   location: PropTypes.shape({
-    hash: PropTypes.string,
+    pathname: PropTypes.string,
   }),
 };
