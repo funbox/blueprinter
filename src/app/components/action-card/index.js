003a9e4b19380e06b5fc5e103ec29877dcf4de73
Ge11ert <gellert94@mail.ru>
2019-08-19 16:01:06 +0300

    Add style modifiers action-card_type_method

--- src/app/components/action-card/index.js
+++ src/app/components/action-card/index.js
@@ -12,3 +12,11 @@ require('./__formatted-href/action-card__formatted-href.scss');
 require('./__method/action-card__method.scss');
 require('./__title/action-card__title.scss');
 require('./__content/action-card__content.scss');
+
+require('./_type/_delete/action-card_type_delete.scss');
+require('./_type/_get/action-card_type_get.scss');
+require('./_type/_head/action-card_type_head.scss');
+require('./_type/_options/action-card_type_options.scss');
+require('./_type/_patch/action-card_type_patch.scss');
+require('./_type/_post/action-card_type_post.scss');
+require('./_type/_put/action-card_type_put.scss');
