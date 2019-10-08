b306f2432f47ab00d6b4b9a60debc76de5ff60b1
Ge11ert <gellert94@mail.ru>
2019-10-08 17:10:41 +0300

    Suppress eslint rule import/no-unresolved for a generated file

--- src/app/source/index.js
+++ src/app/source/index.js
@@ -1,6 +1,8 @@
 let source = {}; // eslint-disable-line
 
 if (ENV !== 'production') {
+  // файл создаётся автоматически, см. npm-скрипт "make-stub"
+  // eslint-disable-next-line import/no-unresolved
   source = require('./refract.json');
 }
 
