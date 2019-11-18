baacc5f9e48075f65458f16a8efce3d4462dc9a5
Ge11ert <gellert94@mail.ru>
2019-11-18 15:05:33 +0300

    Add search field

---
+++ src/app/components/search-field/index.js
@@ -0,0 +1,17 @@
+export { default } from './search-field';
+
+require('../mark/mark.scss');
+
+require('./__clear-button/search-field__clear-button.scss');
+require('./__dropdown/search-field__dropdown.scss');
+require('./__field/search-field__field.scss');
+require('./__input/search-field__input.scss');
+require('./__message/search-field__message.scss');
+require('./__more-stories/search-field__more-stories.scss');
+require('./__option/search-field__option.scss');
+require('./__option-list/search-field__option-list.scss');
+require('./__option-text/search-field__option-text.scss');
+
+require('./search-field.scss');
+
+require('./_open/search-field_open.scss');
---
+++ src/app/components/search-field/search-field.jsx
@@ -0,0 +1,278 @@
+/* eslint-disable react/no-danger */
+import Button from 'fb-base-blocks/button';
+import Link from 'fb-base-blocks/link';
+import TextField from 'app/components/text-field';
+
+const propTypes = {
+  mods: PropTypes.object,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      value: PropTypes.string,
+    }),
+  ),
+  onSearch: PropTypes.func,
+  onSelect: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onShowMoreButtonClick: PropTypes.func,
+};
+
+const defaultProps = {
+  mods: {},
+  items: [],
+  onSearch: undefined,
+  onSelect: undefined,
+  onKeyDown: undefined,
+  onShowMoreButtonClick: undefined,
+};
+
+class SearchField extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.defaultState = {
+      open: false,
+      filterString: '',
+      highlightedItem: null,
+    };
+
+    this.state = { ...this.defaultState };
+    this.dropdown = React.createRef();
+    this.input = React.createRef();
+
+    this.onSearch = this.onSearch.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.closeOnClickAround = this.closeOnClickAround.bind(this);
+    this.select = this.select.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('click', this.closeOnClickAround);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.closeOnClickAround);
+  }
+
+  onSearch(value) {
+    const {
+      items,
+      onSearch,
+    } = this.props;
+
+    this.setState({ filterString: value });
+
+    if (!value) {
+      this.close();
+      return;
+    }
+
+    if (!!value && items) {
+      this.setState({ open: true });
+    }
+
+    if (onSearch) {
+      onSearch(value);
+    }
+  }
+
+  onKeyDown(event) {
+    const { items, onKeyDown } = this.props;
+    const { highlightedItem } = this.state;
+    const itemsCount = items.length;
+
+    if (itemsCount === 0) {
+      this.setState({ highlightedItem: null });
+      return;
+    }
+
+    const itemIndex = items.indexOf(highlightedItem);
+    switch (event.key) {
+      case 'ArrowDown':
+        this.setState({ highlightedItem: (itemIndex === itemsCount - 1 ? items[0] : items[itemIndex + 1]) });
+        break;
+
+      case 'ArrowUp':
+        this.setState({ highlightedItem: (itemIndex === 0 ? items[itemsCount - 1] : items[itemIndex - 1]) });
+        break;
+
+      case 'Enter':
+        if (!highlightedItem) return;
+        this.select(highlightedItem);
+        if (onKeyDown) onKeyDown(highlightedItem);
+        break;
+
+      default:
+        this.setState({ highlightedItem: null });
+    }
+  }
+
+  select(item) {
+    const { onSelect } = this.props;
+
+    if (onSelect) {
+      onSelect(item);
+    }
+    this.close();
+  }
+
+  close() {
+    const { open } = this.state;
+
+    if (!open) return;
+    this.setState({ ...this.defaultState });
+  }
+
+  closeOnClickAround(e) {
+    const dropdownElement = this.dropdown.current;
+    const inputElement = this.input.current;
+    const dropdownClick = dropdownElement && dropdownElement.contains(e.target);
+    const inputClick = inputElement && inputElement.parentElement.contains(e.target);
+
+    if (!(dropdownClick || inputClick)) {
+      this.close();
+    }
+  }
+
+  render() {
+    const {
+      open,
+      filterString,
+      highlightedItem,
+    } = this.state;
+
+    const {
+      items,
+      onShowMoreButtonClick,
+    } = this.props;
+
+    const textFieldProps = Object.assign({}, this.props);
+    const textFieldMods = textFieldProps.mods || {};
+
+    delete textFieldProps.mods;
+    delete textFieldProps.id;
+    delete textFieldProps.mix;
+
+    const defaultMods = { open };
+    const displayedItems = items.slice(0, 10);
+    const showMoreStories = items.length > displayedItems.length;
+
+    return (
+      <div className={b('search-field', this.props, defaultMods)}>
+        <TextField
+          mods={{
+            type: 'search',
+            theme: 'standard',
+            ...textFieldMods,
+          }}
+          id="search-field-input"
+          mix={b('search-field__field')}
+          input={{
+            mix: b('search-field__input'),
+            onKeyDown: this.onKeyDown,
+            ref: this.input,
+          }}
+          value={filterString}
+          onChange={this.onSearch}
+          placeholder="Поиск"
+          {...textFieldProps}
+        >
+          <Button
+            type="reset"
+            mix={[b('search-field__clear-button')]}
+            mods={{
+              onlyIcon: true,
+            }}
+            htmlFor="search-field-input"
+            text="Очистить поле"
+            onClick={() => this.onSearch('')}
+          />
+        </TextField>
+
+        <div
+          className={b('search-field__dropdown')}
+          ref={this.dropdown}
+        >
+          {
+            displayedItems.length > 0 && (
+              <>
+                <ul className={b('search-field__option-list')}>
+                  {
+                    displayedItems.map(item => {
+                      const highlighted = highlightedItem && highlightedItem.value === item.value;
+                      const text = getHighlightedSuggestion(item.label, filterString);
+                      return (
+                        <li
+                          key={item.value}
+                          className={b('search-field__option', { mods: { highlighted, type: item.type } })}
+                        >
+                          {!(item.to || item.href) && (
+                            <Button
+                              onClick={() => this.select(item)}
+                              mix={b('search-field__option-text')}
+                            >
+                              <span
+                                className={b('button__text')}
+                                dangerouslySetInnerHTML={{ __html: text }}
+                              />
+                            </Button>
+                          )}
+
+                          {(item.to || item.href) && (
+                            <Link
+                              onClick={() => this.select(item)}
+                              mix={b('search-field__option-text')}
+                              {...item}
+                            >
+                              <span
+                                className={b('link__text')}
+                                dangerouslySetInnerHTML={{ __html: text }}
+                              />
+                            </Link>
+                          )}
+                        </li>
+                      );
+                    })
+                  }
+                </ul>
+
+                {
+                  showMoreStories && (
+                    <Button
+                      mix={[b('search-field__more-stories')]}
+                      onClick={onShowMoreButtonClick}
+                    >
+                      Показать больше
+                    </Button>
+                  )
+                }
+              </>
+            )
+          }
+
+          {
+            displayedItems.length === 0 && (
+              <p className={b('search-field__message')}>
+                Ничего не найдено
+              </p>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+SearchField.propTypes = propTypes;
+SearchField.defaultProps = defaultProps;
+
+export default SearchField;
+
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function getHighlightedSuggestion(suggestion, filterString) {
+  const filterRegexp = new RegExp(escapeRegExp(filterString), 'gi');
+  return suggestion.replace(filterRegexp, '<mark class="mark">$&</mark>');
+}
