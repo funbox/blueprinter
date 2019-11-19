f0bb90d388809a31101ce1f7b8b9867aa72c6c6d
Ge11ert <gellert94@mail.ru>
2019-11-19 17:56:11 +0300

    Add base component checkbox-field

---
+++ src/fb-base-blocks/checkbox-field/checkbox-field.jsx
@@ -0,0 +1,116 @@
+import uniqid from 'uniqid';
+
+export const propTypes = {
+  id: PropTypes.string,
+  mods: PropTypes.shape({
+    for: PropTypes.string,
+    theme: PropTypes.string,
+    type: PropTypes.string,
+    readonly: PropTypes.bool,
+    disabled: PropTypes.bool,
+    checked: PropTypes.bool,
+    focused: PropTypes.bool,
+    validationState: PropTypes.oneOf(['error', 'success']),
+  }),
+  name: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+};
+
+const defaultProps = {
+  mods: {},
+  id: undefined,
+  name: null,
+  value: undefined,
+  onFocus: undefined,
+  onBlur: undefined,
+};
+
+class CheckboxField extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      focused: false,
+    };
+
+    this.defaultId = uniqid.time('checkbox');
+
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onFocus() {
+    this.setState({
+      focused: true,
+    });
+
+    if (this.props.onFocus) {
+      this.props.onFocus();
+    }
+  }
+
+  onBlur() {
+    this.setState({
+      focused: false,
+    });
+
+    if (this.props.onBlur) {
+      this.props.onBlur();
+    }
+  }
+
+  onChange(event) {
+    this.props.onChange(event.target.checked);
+  }
+
+  render() {
+    const {
+      id = this.defaultId,
+      children,
+      name,
+      value,
+      mods,
+    } = this.props;
+
+    const {
+      focused,
+    } = this.state;
+
+    mods.focused = focused;
+
+    return (
+      <div className={b('checkbox-field', this.props)}>
+        <input
+          className={b('checkbox-field__input')}
+          id={id}
+          type="checkbox"
+          value={value}
+          name={name}
+          checked={mods.checked}
+          disabled={mods.disabled}
+          readOnly={mods.readonly}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          onChange={this.onChange}
+        />
+
+        <label
+          className={b('checkbox-field__label')}
+          htmlFor={id}
+        >
+          {children}
+        </label>
+      </div>
+    );
+  }
+}
+
+CheckboxField.propTypes = propTypes;
+CheckboxField.defaultProps = defaultProps;
+
+export default CheckboxField;
---
+++ src/fb-base-blocks/checkbox-field/index.js
@@ -0,0 +1,6 @@
+export { default, propTypes } from './checkbox-field';
+
+require('./__input/checkbox-field__input.scss');
+require('./__label/checkbox-field__label.scss');
+
+require('./_disabled/checkbox-field_disabled.scss');
