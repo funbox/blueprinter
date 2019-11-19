fd53fb1bac83f2109552fb35bd43b10290c4df11
Ge11ert <gellert94@mail.ru>
2019-11-19 17:58:29 +0300

    Add styling for control elements in the search filter

--- src/fb-base-blocks/checkbox-field/index.js
+++ src/fb-base-blocks/checkbox-field/index.js
@@ -4,3 +4,5 @@ require('./__input/checkbox-field__input.scss');
 require('./__label/checkbox-field__label.scss');
 
 require('./_disabled/checkbox-field_disabled.scss');
+
+require('./_for/_search-filter/checkbox-field_for_search-filter.scss');
