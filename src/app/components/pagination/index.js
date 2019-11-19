7f5e833540b436e73a3b4a7c1fe44a484afaeaab
Ge11ert <gellert94@mail.ru>
2019-11-19 17:51:58 +0300

    Add pagination for search results

---
+++ src/app/components/pagination/index.js
@@ -0,0 +1,11 @@
+export { default } from './paginaton';
+
+require('./__button/pagination__button.scss');
+require('./__control/pagination__control.scss');
+require('./__control/_disabled/pagination__control_disabled.scss');
+require('./__control/_type/_next/pagination__control_type_next.scss');
+require('./__control/_type/_previous/pagination__control_type_previous.scss');
+require('./__item/_current/pagination__item_current.scss');
+require('./__list/pagination__list.scss');
+
+require('./pagination.scss');
