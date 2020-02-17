8f3a9426bd06d94d589e99d02cc4c30ed153ae19
Ge11ert <gellert94@mail.ru>
2020-02-17 22:52:07 +0300

    Add new implementation of collapsible menu section

---
+++ src/app/components/menu-collapsible-section/index.js
@@ -0,0 +1,7 @@
+export { default } from './menu-collapsible-section';
+
+require('./__content/menu-collapsible-section__content.scss');
+require('./__heading/menu-collapsible-section__heading.scss');
+require('./__switcher/menu-collapsible-section__switcher.scss');
+
+require('./_collapsed/menu-collapsible-section_collapsed.scss');
---
+++ src/app/components/menu-collapsible-section/menu-collapsible-section.jsx
@@ -0,0 +1,139 @@
+import { SlideToggle } from 'react-slide-toggle';
+import { withRouter } from 'react-router-dom';
+
+import Button from 'fb-base-blocks/button';
+import SideNestedMenu from '../side-nested-menu';
+
+const propTypes = {
+  route: PropTypes.string,
+  content: PropTypes.arrayOf(PropTypes.object),
+  currentLevel: PropTypes.number,
+  nextLevel: PropTypes.number,
+  visible: PropTypes.bool,
+  children: PropTypes.node,
+  location: PropTypes.shape({
+    pathname: PropTypes.string,
+  }),
+};
+
+class MenuCollapsibleSection extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const {
+      route,
+      location: { pathname },
+    } = props;
+
+    const current = matchRoute(route, pathname);
+
+    this.state = {
+      collapsed: !current,
+      content: null,
+    };
+
+    this.nestedMenuRef = React.createRef();
+
+    this.toggleCollapsed = this.toggleCollapsed.bind(this);
+  }
+
+  componentDidMount() {
+    const { content } = this.props;
+    const hasContent = !!content && content.length > 0;
+
+    if (hasContent && this.autoBuildContent()) {
+      this.setState({ content }, () => {
+        this.nestedMenuRef.current.createContent();
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { content, visible } = this.props;
+    const becameVisible = visible && !prevProps.visible;
+    const becameHidden = !visible && prevProps.visible;
+
+    if (this.autoBuildContent()) {
+      return;
+    }
+
+    if (becameVisible) {
+      const hasContent = !!content && content.length > 0;
+      if (hasContent) {
+        this.setState({ content }, () => {
+          this.nestedMenuRef.current.createContent();
+        });
+      }
+    }
+
+    if (becameHidden) {
+      this.setState({ content: null });
+    }
+  }
+
+  toggleCollapsed() {
+    this.setState(prevState => ({
+      collapsed: !prevState.collapsed,
+    }));
+  }
+
+  autoBuildContent() {
+    return this.props.currentLevel === 1;
+  }
+
+  render() {
+    const {
+      route,
+      nextLevel,
+      children,
+    } = this.props;
+    const { collapsed, content } = this.state;
+
+    return (
+      <SlideToggle
+        bestPerformance
+        collapsed={collapsed}
+        onCollapsed={this.toggleCollapsed}
+        onExpanding={this.toggleCollapsed}
+      >
+        {({ onToggle, setCollapsibleElement }) => (
+          <div className={b('menu-collapsible-section', { mods: { collapsed } })}>
+            <div className={b('menu-collapsible-section__heading')}>
+              {children}
+
+              <Button
+                mix={b('menu-collapsible-section__switcher')}
+                mods={{
+                  onlyIcon: true,
+                }}
+                text={collapsed ? 'Раскрыть' : 'Свернуть'}
+                onClick={onToggle}
+              />
+            </div>
+
+            <div className={b('menu-collapsible-section__content')} ref={setCollapsibleElement}>
+              { content && (
+                <SideNestedMenu
+                  content={content}
+                  level={nextLevel}
+                  parentRoute={route}
+                  ref={this.nestedMenuRef}
+                  visible={!collapsed}
+                />
+              )}
+            </div>
+          </div>
+        )}
+      </SlideToggle>
+    );
+  }
+}
+
+MenuCollapsibleSection.propTypes = propTypes;
+
+export default withRouter(MenuCollapsibleSection);
+
+function matchRoute(route, pathname) {
+  const locationRegex = new RegExp(`^${route}.*`);
+  return pathname !== '/' && locationRegex.test(pathname);
+}
