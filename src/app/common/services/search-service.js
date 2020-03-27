4ef7794b62de1cfc77b38d323b5202d716214ed4
Ge11ert <gellert94@mail.ru>
2020-03-27 15:58:15 +0300

    Add the ability to specify search modifier

--- src/app/common/services/search-service.js
+++ src/app/common/services/search-service.js
@@ -4,6 +4,16 @@ const defaultFound = Object.freeze({
   action: [],
 });
 
+const possibleModifiers = {
+  type: ['group', 'action', 'resource'],
+  method: ['delete', 'get', 'head', 'options', 'patch', 'post', 'put', 'message'],
+};
+
+const modifiersRegexps = Object.keys(possibleModifiers).map(modifierType => {
+  const modifierValues = possibleModifiers[modifierType];
+  return new RegExp(`^(${modifierType}):(${modifierValues.join('|')})\\s+(.+)`, 'i');
+});
+
 class SearchService {
   constructor(groups, resources, actions) {
     this.sourceGroups = groups;
@@ -13,11 +23,14 @@ class SearchService {
     this.found = defaultFound;
   }
 
-  search(searchQuery) {
-    if (!searchQuery) return [];
-    const [groupHigh, groupMid, groupLow] = searchInSource(searchQuery, this.sourceGroups, 'group');
-    const [resHigh, resMid, resLow] = searchInSource(searchQuery, this.sourceResources, 'resource');
-    const [actionHigh, actionMid, actionLow] = searchInSource(searchQuery, this.sourceActions, 'action');
+  search(rawSearchQuery) {
+    if (!rawSearchQuery) return [];
+
+    const { searchQuery, modifiers } = extractModifiers(rawSearchQuery);
+
+    const [groupHigh, groupMid, groupLow] = searchInSource(searchQuery, this.sourceGroups, 'group', modifiers);
+    const [resHigh, resMid, resLow] = searchInSource(searchQuery, this.sourceResources, 'resource', modifiers);
+    const [actionHigh, actionMid, actionLow] = searchInSource(searchQuery, this.sourceActions, 'action', modifiers);
 
     const highPriorityMatch = groupHigh.concat(resHigh, actionHigh);
     const midPriorityMatch = groupMid.concat(resMid, actionMid);
@@ -45,17 +58,30 @@ class SearchService {
   }
 }
 
-function searchInSource(query, source, sourceType) {
+function searchInSource(query, source, sourceType, searchModifiers) {
   const highPriorityMatch = [];
   const midPriorityMatch = [];
   const lowPriorityMatch = [];
 
+  const emptyResponse = [[], [], []];
+
+  if (searchModifiers.type !== undefined && searchModifiers.type !== sourceType) {
+    return emptyResponse;
+  }
+
+  if (searchModifiers.method !== undefined && sourceType !== 'action') {
+    return emptyResponse;
+  }
+
   source.forEach((sourceItem) => {
+    const titleMatch = findMatch(sourceItem.title, query);
     const descriptionEl = sourceItem.content ? sourceItem.content.find(el => el.element === 'copy') : null;
     const description = descriptionEl ? descriptionEl.content : null;
-    const titleMatch = findMatch(sourceItem.title, query);
     const descriptionMatch = description && findMatch(description, query);
     const method = sourceType === 'action' ? getMethod(sourceItem) : null;
+    const methodMatch = searchModifiers.method === undefined
+      ? true
+      : method.toLowerCase() === searchModifiers.method.toLowerCase();
 
     const searchItem = {
       label: sourceItem.title,
@@ -66,6 +92,10 @@ function searchInSource(query, source, sourceType) {
     };
 
     if (sourceType === 'action') {
+      if (!methodMatch) {
+        return;
+      }
+
       const href = sourceItem.attributes.href;
       const hrefMatch = href && findMatch(href, query);
 
@@ -102,6 +132,17 @@ function findMatch(source, substr) {
   const lcSubstr = substr.toLowerCase();
   const words = lcSubstr.split(' ').filter(Boolean);
   return words.every((word) => lcSource.includes(word));
+}
+
+function extractModifiers(rawSearchQuery) {
+  return modifiersRegexps.reduce((result, regex) => {
+    if (regex.test(rawSearchQuery)) {
+      const [, type, value, extractedQuery] = regex.exec(rawSearchQuery);
+      result.searchQuery = extractedQuery;
+      result.modifiers[type] = value;
+    }
+    return result;
+  }, { searchQuery: rawSearchQuery, modifiers: {} });
 }
 
 export default SearchService;
