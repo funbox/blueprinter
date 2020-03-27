4ac23e268bd503c24c2cff59b1e11932307f9961
Ge11ert <gellert94@mail.ru>
2020-03-27 12:27:55 +0300

    Delete irrelevant methods and functions from search service

--- src/app/common/services/search-service.js
+++ src/app/common/services/search-service.js
@@ -1,6 +1,3 @@
-const hrefRegex = /(\/\S*)/i;
-const methodRegex = /(delete|get|head|options|patch|post|put)/i;
-
 const defaultFound = Object.freeze({
   group: [],
   resource: [],
@@ -16,30 +13,6 @@ class SearchService {
     this.found = defaultFound;
   }
 
-  search(searchQuery) {
-    if (!searchQuery) return [];
-
-    if (hrefRegex.test(searchQuery) || methodRegex.test(searchQuery)) {
-      const actionMatch = searchInAction(searchQuery, this.sourceActions);
-      this.found = {
-        ...defaultFound,
-        action: actionMatch,
-      };
-      return actionMatch;
-    }
-    const groupMatch = searchInSource(searchQuery, this.sourceGroups, 'group');
-    const resourceMatch = searchInSource(searchQuery, this.sourceResources, 'resource');
-    const actionMatch = searchInSource(searchQuery, this.sourceActions, 'action');
-    this.found = {
-      ...defaultFound,
-      group: groupMatch,
-      resource: resourceMatch,
-      action: actionMatch,
-    };
-    const fullMatch = groupMatch.concat(resourceMatch, actionMatch);
-    return fullMatch;
-  }
-
   applyFilter(activeFilters) {
     const fullMatch = Object.entries(activeFilters).reduce((acc, entry) => {
       const [type, isActive] = entry;
@@ -51,60 +24,6 @@ class SearchService {
     }, []);
     return fullMatch;
   }
-}
-
-function searchInSource(query, source, sourceType) {
-  const searchResult = source.reduce((result, sourceItem) => {
-    const descriptionEl = sourceItem.content ? sourceItem.content.find(el => el.element === 'copy') : null;
-    const description = descriptionEl ? descriptionEl.content : null;
-    const titleMatch = findMatch(sourceItem.title, query);
-    const descriptionMatch = description && findMatch(description, query);
-    let method = null;
-    if (sourceType === 'action') {
-      method = sourceItem.type === 'message' ? 'message' : sourceItem.attributes.method;
-    }
-
-    if (titleMatch || descriptionMatch) {
-      return result.concat([{
-        label: sourceItem.title,
-        value: sourceItem.route,
-        to: sourceItem.route,
-        type: sourceType,
-        method,
-      }]);
-    }
-
-    return result;
-  }, []);
-  return searchResult;
-}
-
-function searchInAction(query, source) {
-  const methodRegexpMatch = methodRegex.exec(query);
-  const hrefRegexpMatch = hrefRegex.exec(query);
-
-  const methodFromQuery = methodRegexpMatch && methodRegexpMatch[1] || null;
-  const hrefFromQuery = hrefRegexpMatch && hrefRegexpMatch[1] || null;
-
-  const searchResult = source.reduce((result, sourceItem) => {
-    const titleMatch = findMatch(sourceItem.title, query);
-    const methodMatch = methodFromQuery ? findMatch(sourceItem.attributes.method, methodFromQuery) : null;
-    const hrefMatch = hrefFromQuery ? findMatch(sourceItem.attributes.href, hrefFromQuery) : null;
-    const methodHrefMatch = methodFromQuery && hrefFromQuery ? (methodMatch && hrefMatch) : (methodMatch || hrefMatch);
-
-    if (methodHrefMatch || titleMatch) {
-      return result.concat([{
-        label: hrefMatch ? sourceItem.attributes.href : sourceItem.title,
-        value: sourceItem.route,
-        to: sourceItem.route,
-        type: 'action',
-        method: sourceItem.attributes.method,
-      }]);
-    }
-    return result;
-  }, []);
-
-  return searchResult;
 }
 
 function findMatch(source, substr) {
