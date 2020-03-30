78745740c2e47a432b996a6c869e8bdd07b92e73
Ge11ert <gellert94@mail.ru>
2020-03-30 14:13:49 +0300

    Delete components used on search results page

--- src/app/components/serp-list/index.js
+++
@@ -1,8 +0,0 @@
-export { default, propTypes as serpListProps, searchItemType } from './serp-list';
-
-require('./__badge/serp-list__badge.scss');
-require('./__item/serp-list__item.scss');
-require('./__label/serp-list__label.scss');
-require('./__title/serp-list__title.scss');
-
-require('./serp-list.scss');
