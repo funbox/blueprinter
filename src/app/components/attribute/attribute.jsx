8cc36b78973077884543d9895538dd16bc90db73
Ge11ert <gellert94@mail.ru>
2020-04-16 12:16:00 +0300

    Render unfolded attributes on the manual search page

--- src/app/components/attribute/attribute.jsx
+++ src/app/components/attribute/attribute.jsx
@@ -1,11 +1,13 @@
 import SlideToggle from 'react-slide-toggle';
+import ViewContext from 'app/components/app/view-context';
+
 import Attribute__Row from './__row';
 
 class Attribute extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
 
-    this.state = { collapsed: true };
+    this.state = { collapsed: context !== 'expanded' };
     this.toggleCollapsedState = this.toggleCollapsedState.bind(this);
   }
 
@@ -24,11 +26,30 @@ class Attribute extends React.Component {
       mods = {},
       mix = [],
     } = this.props;
+    const viewMode = this.context;
 
     const { hasChildren } = mods;
 
     if (hasChildren) {
       mods.collapsed = this.state.collapsed;
+    }
+
+    if (viewMode === 'expanded') {
+      mods.static = true;
+      return (
+        <div className={b('attribute', { mods, mix })}>
+          <Attribute__Row
+            attribute={attribute}
+            parentType={parentType}
+          />
+
+          { !!hasChildren && (
+            <div className="attribute__children">
+              {renderNestedAttrs()}
+            </div>
+          )}
+        </div>
+      );
     }
 
     return (
@@ -37,7 +58,7 @@ class Attribute extends React.Component {
         bestPerfomance
         onExpanded={this.toggleCollapsedState}
         onCollapsing={this.toggleCollapsedState}
-        collapsed
+        collapsed={viewMode !== 'expanded'}
       >
         {({ onToggle, setCollapsibleElement }) => (
           <div className={b('attribute', { mods, mix })}>
@@ -56,6 +77,8 @@ class Attribute extends React.Component {
     );
   }
 }
+
+Attribute.contextType = ViewContext;
 
 Attribute.propTypes = {
   attribute: PropTypes.shape({
