521693c7d557c94e0d62b0a235a18b7d87310083
natrey <natandreychenko@gmail.com>
2020-11-23 14:41:53 +0300

    Add 'file' to STANDARD_TYPES

--- src/app/constants/defaults.js
+++ src/app/constants/defaults.js
@@ -2,4 +2,4 @@ export const API_DEFAULT_TITLE = 'API';
 export const GROUP_DEFAULT_TITLE = 'Resource Group';
 export const RESOURCE_DEFAULT_TITLE = 'Resource';
 export const MESSAGE_DEFAULT_TITLE = 'Message';
-export const STANDARD_TYPES = ['number', 'string', 'boolean', 'array', 'enum', 'object'];
+export const STANDARD_TYPES = ['number', 'string', 'boolean', 'array', 'enum', 'object', 'file'];
