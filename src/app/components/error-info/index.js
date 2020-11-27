aeffd0538640f6b75fcbf73ed690a2ef4576fa90
Ge11ert <gellert94@mail.ru>
2020-11-27 16:04:06 +0300

    Delete old css for the error-info component

--- src/app/components/error-info/index.js
+++ src/app/components/error-info/index.js
@@ -1,6 +1,1 @@
 export { default, errorProps } from './error-info';
-
-require('./__message/error-info__message.scss');
-require('./__source-line/error-info__source-line.scss');
-require('./__summary-line/_for/_source-lines/error-info__summary-line_for_source-lines.scss');
-require('./__summary-text/error-info__summary-text.scss');
