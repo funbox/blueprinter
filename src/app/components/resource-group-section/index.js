32eb1a28ec2ad0e08a6d23c8e3c1f09a17168f7f
natrey <natandreychenko@gmail.com>
2021-01-13 17:25:22 +0300

    Add counters to sections and sub-sections

--- src/app/components/resource-group-section/index.js
+++ src/app/components/resource-group-section/index.js
@@ -1,5 +1,6 @@
 export { default } from './resource-group-section';
 
+require('./resource-group-section.scss');
 require('./__heading/resource-group-section__heading.scss');
 require('./__anchor/resource-group-section__anchor.scss');
 require('./__description/resource-group-section__description.scss');
--- src/app/components/resource/index.js
+++ src/app/components/resource/index.js
@@ -1,5 +1,6 @@
 export { default } from './resource';
 
+require('./resource.scss');
 require('./__heading/resource__heading.scss');
 require('./__anchor/resource__anchor.scss');
 require('./__description/resource__description.scss');
