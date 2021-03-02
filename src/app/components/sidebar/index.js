263cfe7bf939864bdf8516bb5fa301a624adc539
GunAlv <44808973+GunAlv@users.noreply.github.com>
2021-03-02 15:56:54 +0700

    Add sidebar styling

--- src/app/components/sidebar/index.js
+++ src/app/components/sidebar/index.js
@@ -1,1 +1,9 @@
 export { default } from './sidebar';
+
+require('./__toggle/sidebar__toggle.scss');
+require('./__resizable/sidebar__resizable.scss');
+
+require('./sidebar.scss');
+
+require('./_closed/sidebar_closed.scss');
+require('./_for/_sandbox/sidebar_for_sandbox.scss');
