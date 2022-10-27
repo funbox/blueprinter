2905cac58c4b20f05503c1cfefb3a554d67b985f
Ge11ert <gellert94@mail.ru>
2022-10-27 14:11:57 +0300

    Load additional locale data

--- src/app/app.i18n.js
+++ src/app/app.i18n.js
@@ -1,6 +1,11 @@
 import { i18n } from '@lingui/core';
+import { en, ru } from 'make-plural/plurals';
 
 const supportedLocales = ['en', 'ru'];
+const localeData = {
+  en: { plurals: en },
+  ru: { plurals: ru },
+};
 
 const localeCatalog = window.i18n ? window.i18n.messages : undefined;
 const definedLocale = document.documentElement.lang;
@@ -9,6 +14,7 @@ const activeLocale = supportedLocales.includes(definedLocale) ? definedLocale : 
 if (localeCatalog) {
   i18n.load(activeLocale, localeCatalog);
 }
+i18n.loadLocaleData(activeLocale, localeData[activeLocale]);
 i18n.activate(activeLocale);
 
 export default i18n;
