eddab0e6454cd0549002bb01dd5df98be8b38d66
Ge11ert <gellert94@mail.ru>
2023-05-25 16:43:27 +0300

    Restore lost trim in stringify function

--- src/app/common/utils/helpers/stringify.js
+++ src/app/common/utils/helpers/stringify.js
@@ -1,5 +1,5 @@
 import { isString } from 'app/common/utils/helpers/guards';
 
 export default function stringify(input) {
-  return isString(input) ? input : JSON.stringify(input, null, 2);
+  return isString(input) ? input.trim() : JSON.stringify(input, null, 2);
 }
