0d249d982ffbf3f6b3f3fe7a1833620e31245595
Alexander Solovev <solovev.dev@gmail.com>
2023-10-05 13:21:31 +0300

    Update cookies helper

--- src/app/common/utils/helpers/cookies.js
+++ src/app/common/utils/helpers/cookies.js
@@ -1,28 +1,52 @@
+const DEFAULT_OPTIONS = {
+  domain: window.location.hostname.split('.').slice(-2).join('.'),
+};
+
 const getCookie = ({ name }) => {
   const regex = `(?:^|; )${name.replace(/([.$?*|{}()[\]\\/+^])/g, '\\$1')}=([^;]*)`;
   const matches = document.cookie.match(new RegExp(regex));
   return matches ? decodeURIComponent(matches[1]) : undefined;
 };
 
-const setCookie = ({ name, value, options = { path: '/' } }) => {
+const setCookie = ({
+  name,
+  value,
+  options = {},
+}) => {
   const encodedValue = encodeURIComponent(value);
   let updatedCookie = `${name}=${encodedValue}`;
 
-  if (!options.expires) {
+  const updatedOptions = {
+    ...DEFAULT_OPTIONS,
+    ...options,
+  };
+
+  if (!updatedOptions.expires) {
     const date = new Date();
     date.setFullYear(2100);
-    options.expires = date.toUTCString();
+    updatedOptions.expires = date.toUTCString();
   }
 
-  Object.keys(options).forEach(propKey => {
-    const propValue = options[propKey];
+  Object.keys(updatedOptions).forEach(propKey => {
+    const propValue = updatedOptions[propKey];
     updatedCookie += `; ${propKey}=${propValue}`;
   });
 
   document.cookie = updatedCookie;
 };
 
+const removeCookie = ({ name }) => {
+  setCookie({
+    name,
+    value: '',
+    options: {
+      expires: new Date(0).toUTCString(),
+    },
+  });
+};
+
 export default {
   get: getCookie,
   set: setCookie,
+  remove: removeCookie,
 };
